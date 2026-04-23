c67a6ca1ae79379569f0319f5832f35c2c1d46c1
youngsik <syssys6831@gmail.com>
2026-04-23 19:49:17 +0900

    swagger 초반 세팅 (#6)
    * feat/ swagger 패키지 설치
    
    * feat/ swagger 설정파일 생성 및 서버 연결
    
    * feat/ ts-node-dev 대신 tsx 도입
    
    * fix: index.ts->app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger.js";
 
 dotenv.config();
 
@@ -10,10 +12,13 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("🚀 Pack-IT 백엔드 서버가 정상적으로 켜져 있습니다!");
 });
 
 app.listen(PORT, () => {
   console.log(`서버가 포트 ${PORT} 에서 성공적으로 실행 중입니다!`);
+  console.log(`📚 API 명세서 주소: http://localhost:${PORT}/api-docs`);
 });
