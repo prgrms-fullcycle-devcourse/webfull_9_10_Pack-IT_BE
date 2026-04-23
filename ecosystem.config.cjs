cc37b3577f01eba44e5d1d0a6be8d9f090c48c74
youngsik <syssys6831@gmail.com>
2026-04-23 20:06:08 +0900

    Feat/swagger initial (#8)
    * feat/ swagger 패키지 설치
    
    * feat/ swagger 설정파일 생성 및 서버 연결
    
    * feat/ ts-node-dev 대신 tsx 도입, 기본파일 app.js 설정

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "packit-backend",
-      script: "./dist/index.js",
+      script: "./dist/app.js",
       instances: 1,
       exec_mode: "fork",
     },
---
+++ src/config/swagger.ts
@@ -0,0 +1,26 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Pack-IT API 명세서",
+      version: "1.0.0",
+      description: "Pack-IT 백엔드 API 문서입니다.",
+    },
+    servers: [
+      {
+        url: "http://54.180.159.211:8080",
+        description: "배포 서버",
+      },
+      {
+        url: "http://localhost:8080",
+        description: "로컬 테스트 서버",
+      },
+    ],
+  },
+  //주석을 읽어올 경로
+  apis: ["./src/routes/*.ts", "./src/controllers/*.ts"],
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
