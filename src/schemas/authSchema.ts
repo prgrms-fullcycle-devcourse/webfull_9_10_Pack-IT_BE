a52a73b5d51956516e7cbb852106c6b617310af3
youngsik <syssys6831@gmail.com>
2026-05-08 15:32:04 +0900

    feat: 카카오 연동 로그인 시 state로 nanoid 받아서 기존 nanoid 확인 feat: 컨트롤러 생성하여 카카오 콜백 처리 분리

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -3,5 +3,6 @@ import { z } from "zod";
 export const kakaoCallbackSchema = z.object({
   query: z.object({
     code: z.string({ message: "카카오 인가 코드가 필요합니다." }),
+    state: z.string({ message: "유저 식별용 state(nanoId) 값이 필요합니다." }),
   }),
 });
