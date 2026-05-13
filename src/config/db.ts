230c22579d69ce0c67d8b416b1fcc034cd6b0ae1
youngsik <syssys6831@gmail.com>
2026-05-13 15:26:56 +0900

    fix/prisma_builderror

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
+const { PrismaClient } = pkg as any;
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import fs from "fs";
